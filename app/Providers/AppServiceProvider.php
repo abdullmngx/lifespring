539dcbea9fb7e103a86c5b74c4d3e08e30f0ea88
Muhammad <abdullmng@gmail.com>
2023-01-16 17:48:45 +0100

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        // set the public path to this directory
+        $this->app->bind('path.public', function() {
+            return base_path().'/public_html';
+        });
     }
 
     /**
